9d3cbe1e2d0dea6b1e154f840827bbbcde183740
Anilab17 <anilkumarmadivalar5@gmail.com>
2026-09-24 18:36:10 +0530

    server connection

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-console.log("Server is running");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const connectDB = require('./config/db');
 
+const app = express();
+
+app.use(express.json());
+app.use(cors('*'));
+connectDB()
+
+const PORT = process.env.PORT || 5000;  
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
